68a58f1a70e7b3c497061dacd2742b0427148975
zhiyuan122 <zyu6@ualberta.ca>
2025-01-22 14:45:58 -0700

    update

---
+++ CPP/Easy/14classFriendfunction.cpp
@@ -0,0 +1,44 @@
+/*
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+    {
+    private:
+        int length, breadth, height;
+
+    public:
+        Box
+        {
+            (/* args */);
+            ~Box
+            {
+                ();
+            };
+
+            Box
+            {
+                ::Box
+                {
+                    (/* args */)
+                    {
+                    }
+
+                    Box
+                    {
+                        ::~Box
+                        {
+                            ()
+                            {
+                            }
+
+                            int main()
+                            {
+
+                                return 0;
+                            }
